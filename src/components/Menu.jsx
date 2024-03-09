66083dba0aea65a215ea4085468d6cf62f67eef1
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-03-09 22:34:09 +0100

    activation des lien

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Menu = () => {
   return (
     <div>
       <ul className="menu">
-        <li>Home</li>
-        <li>Add Techno</li>
-        <li>All Technos</li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/add"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            {" "}
+            Add Techno
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="list"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            All Technos
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
