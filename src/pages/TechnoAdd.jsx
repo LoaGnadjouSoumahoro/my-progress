1bdf51acb212e2e28a1893d17865baa0ed1c3734
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-03-09 06:39:54 +0100

    ajout des champs dans la page TechnoAdd

--- src/pages/TechnoAdd.jsx
+++ src/pages/TechnoAdd.jsx
@@ -6,11 +6,28 @@ export const TechnoAdd = () => {
       <h1>Add a Techno</h1>
       <div>
         <form action="">
-          <label htmlFor="techno-name">Name</label>
+          <label htmlFor="techno-name">Name:</label>
           <br />
           <input type="text" name="techno-name" id="techno-name" />
           <br />
-          <select name="techno-categorie" id="techno-categorie"></select>
+          <label htmlFor="techno-category">Category:</label>
+          <br />
+          <select name="techno-category" id="techno-category">
+            <option value=""> Select a category</option>
+            <option value="frontEnd">Front-End</option>
+            <option value="BackEnd">Back-End</option>
+            <option value="fullStack">Full Stack</option>
+            <option value="other">Other</option>
+          </select>
+          <br />
+          <label htmlFor="techno-description">Description:</label>
+          <br />
+          <textarea
+            name="techno-description"
+            id="techno-description"
+            cols="30"
+            rows="10"
+          ></textarea>
           <br />
           <input type="submit" value="Add Techno" />
         </form>
